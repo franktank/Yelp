f453837817ed6c3e86d4d21f1af6d8eb93c73cfe
Franky Liang <frankyliang@Frankys-Mac.local>
2016-01-31 15:57:10 -0500

    added autolayout of imageviews and labels

--- Yelp/YelpClient.swift
+++ Yelp/YelpClient.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 import AFNetworking
-import BDBOAuth1Manager
+import BDBOAuth1Manager		
 
 // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
 let yelpConsumerKey = "vxKwwcR_NMQ7WaEiQBK_CA"
